60e256b4da9b4aaf1d66b555e2e1790036ae898a
ddthj <theddthj@gmail.com>
2019-09-23 00:10:21 -0400

    better scenario file, new parser structure, reworked objects

--- objects.py
+++ objects.py
@@ -1,54 +1,80 @@
 
 class aircraft:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,ref,tail):
+        self.ref = ref
+        self.tail = tail
     def __str__(self):
-        return "Aircraft "+self.name
+        return self.id
 
-class crew:
-    def __init__(self,name):
-        self.name = name
+class group:
+    def __init__(self,ref,people):
+        self.ref = ref #used for parsing only
+        self.people = people
     def __str__(self):
-        return "Crew " +self.name
+        return self.id
 
-class cabin_crew(crew):
-    pass
-
-class flight_crew(crew):
-    pass
+class itinerary:
+    def __init__(self,ident,flights):
+        self.id = ident
+        self.flights = flights
+    def __str__(self):
+        msg = self.id + "\n"
+        for item in self.flights:
+            msg += item + "\n"
+        return msg
+        
+class manifest:
+    def __init__(self,ref,objects):
+        self.ref = ref
+        self.objects = objects
+    
+class person:
+    def __init__(self,ref,ident,role,itinerary = None):
+        self.id = ident
+        self.ref = ref #used for parsing only
+        self.role = role
+    def __str__(self):
+        return self.id
 
 class flight:
-    def __init__(
-        self,
-        name,
+    def __init__(self,
+        ident,
+        manifest,
         aircraft,
-        flight_crew,
         departure_location,
         departure_time,
         arrival_location,
         arrival_time):
 
-        self.name = name
+        self.id = ident
+        self.manifest = manifest
         self.aircraft = aircraft
-        self.flight_crew = flight_crew
+        self.flight_crew = []
+        self.cabin_crew = []
+        self.passengers = []
+        self.nonrev = []
+        self.cargo = []
         self.departure_location = departure_location
         self.departure_time = departure_time
         self.arrival_location = arrival_location
         self.arrival_time = arrival_time
         self.status = "scheduled"
-        self.warnings = []
         
 class location:
-    def __init__(self, name, aircraft, crew):
-        self.name = name
-        self.aircraft = [] if aircraft == None else aircraft
-        self.crew = [] if crew == None else crew
+    def __init__(self, ident, aircraft, flight_crew, cabin_crew, passengers, cargo):
+        self.id = ident
+        self.aircraft = aircraft
+        self.flight_crew = flight_crew
+        self.cabin_crew = cabin_crew
+        self.passengers = passengers
+        self.cargo = cargo
+        
     def __str__(self):
-        msg = str(self.name) + "\n"
-        for aircraft in self.aircraft:
-            msg += str(aircraft) + "\n"
-        for crew in self.crew:
-            msg += str(crew) + "\n"
+        msg = str(self.id) + "\n"
+        msg += "Aircraft: %s" % str( len(self.aircraft))
+        msg += "Pilots: %s" % str( len(self.flight_crew))
+        msg += "Attendants: %s" % str( len(self.cabin_crew))
+        msg += "Passengers: %s" % str( len(self.passengers))
         return msg
 
 class scenario:
--- parser.py
+++ parser.py
@@ -1,72 +1,145 @@
 from objects import *
 
-class parser:
+class gen:
     def __init__(self):
-        self.locations = []
-        self.flights = []
-
-    def handle_obj(self,obj):
-        if obj[0].find("location") != -1:
-            location_name = obj[0].split("location")[1]
-            temp = location(location_name,None,None)
-            for item in obj:
-                if item.find("flight_crew") != -1:
-                    flight_crew_name = line.split("flight_crew")[1]
-                    temp.crew.append(flight_crew(flight_crew_name))
-                elif item.find("aircraft") != -1:
-                    aircraft_name = line.split("aircraft")[1]
-                    temp.aircraft.append(aircraft(aircraft_name))
-                #cabin crew, cargo, nonrev, etc
-            self.locations.append(temp)
+        self.person = 0
+        x = itinerary("a",[])
+    def get(self,role):
+        number = "{:04d}".format(self.person)
+        self.person +=1
+        if role == "flight_crew":
+            return "fc"+number
+        elif role == "cabin_crew":
+            return "cc"+number
+        else:
+            return "px"+number
         
+class parser:
+    def __init__(self,file):
+        self.file = file
+        self.gen = gen()
+        self.raw = self.load(self.file)
+        self.locations = []
+        self.aircraft = []
+        self.people = []
+        self.itineraries = []
+        self.groups = []
+        self.flights = []
+        self.manifests = []
+        
+        if self.raw != None:
+            self.raw_objects = self.split(self.raw)
+            self.parse(self.raw_objects)
+            
+    #turns lists into actual objects
+    def parse(self,obs):
+        #first pass defines all itineraries and aircraft
+        for item in obs:
+            if item[0].find("itinerary") != -1:
+                ident = item[0].split("itinerary")[1]
+                flights = [item[i].strip() for i in range(1,len(item))]
+                self.itineraries.append(itinerary(ident,flights))
+            elif item[0].find("aircraft") != -1:
+                ref = item[0].split("aircraft")[1]
+                tail = None
+                for i in range(1,len(item)):
+                    if item[i].find("tail") != -1:
+                        tail = item[i].split("tail")[1]
+                self.aircraft.append(aircraft(ref,tail))
+
+        #second pass gets people and groups
+        for item in obs:
+            if item[0].find("person") != -1:
+                ref = item[0].split("person")[1]
+                ident = None
+                role = None
+                itinerary_ref = None
+                for i in range(1,len(item)):
+                    if item[i].find("role") != -1:
+                        role = item[i].split("role")[1]
+                    elif item[i].find("itinerary") != -1:
+                        itinerary_ref = item[i].split("itinerary")[1]       
+                self.people.append(person(ref,self.gen.get(role),role,itinerary_ref))
+                
+            elif item[0].find("group") != -1:
+                ref = item[0].split("group")[1]
+                quantity = None
+                itinerary_ref = None
+                role = None
+                for i in range(1,len(item)):
+                    if item[i].find("quantity") != -1:
+                        quantity = item[i].split("quantity")[1]
+                    elif item[i].find("itinerary") != -1:
+                        itinerary_ref = item[i].split("itinerary")[1]
+                    elif item[i].find("role") != -1:
+                        role = item[i].split("role")[1]
+                people = [person(None,self.gen.get(role),role,itinerary_ref) for i in range(int(quantity))]
+                #self.people += people
+                self.groups.append(group(ref,people))
+
+        #third pass gets manifests and locations
+        for item in obs:
+            if item[0].find("manifest") != -1:
+                ref = item[0].split("manifest")[1]
+                include = []
+                for i in range(1,len(item)):
+                    if item[i].find("person") != -1:
+                        ref = item[i].split("person")[1]
+                        include.append(self.person_by_ref(ref))
+                    elif item[i].find("group") != -1:
+                        ref = item[i].split("group")[1]
+                        #include += self.group_by_ref(ref).people
+                self.manifests.append(manifest(ref,include))
+            elif item[0].find("location") != -1:
+                name = item[0].split("location")[1]
+                '''
+                aircraft = []
+                flight_crew = []
+                cabin_crew = []
+                passengers = []
+                cargo = []
+                '''
+                temp = [x for x in item if x.find("\t\t")]
+                for i in range(len(temp)):
+                    if temp[i].find("aircraft"):
+                        pass
+                                     
+                        
+    def group_by_ref(self,ref):
+        for group in self.groups:
+            if group.ref == ref:
+                return group
+        print("couldn't find group %s" % (ref))
+        
+    def person_by_ref(self,ref):
+        for person in self.people:
+            if person.ref == ref:
+                return person
+        print("couldn't find person %s" % (ref))
     
+    #splits a list by strings that aren't indented
+    def split(self,raw):
+        obs = []
+        i = 0
+        for j in range(1,len(raw)):
+            if raw[j].find("\t") == -1:
+                obs.append(raw[i:j])
+                i=j
+        obs.append(raw[i:j])
+        return obs
+                
+    #reads file, puts each valid line into a list
     def load(self,file):
         try:
             with open(file,"r") as f:
-                raw = [line.strip().replace(" ","") for line in f.read().split("\n") if len(line) > 0]
+                #removes spaces, newlines, empty lines, and anything with a "/"
+                raw = [line.replace(" ","") for line in f.read().split("\n") if len(line.strip()) > 0 and line[0] != "/"]
+                return raw
         except Exception as e:
             print(e)
-            return None,None
-
-        obj = 0
-        for i in range(len(raw)):
-            if raw[i].find("location") != -1:
-                if obj != 0:
-                    self.handle_obj(raw[obj:i-1])
-                obj = i
-            elif raw[i].find("flight") != -1 and not raw[i].find("crew") != -1:
-                if obj != 0:
-                    pass#self.handle_obj(raw[obj:i-1])
-                obj = i
-        #self.handle_obj(raw[obj:i])           
-            
-                
-        '''        
-        temp = location("temp",None,None)
-        for line in raw:
-            if line.find("location") != -1:
-                location_name = line.split("location")[1]
-                if temp.name != "temp":
-                    locations.append(temp)
-                temp = location(location_name,None,None)
-            elif line.find("flight_crew") != -1:
-                flight_crew_name = line.split("flight_crew")[1]
-                temp_flight_crew = flight_crew(flight_crew_name)
-                temp.crew.append(temp_flight_crew)
-            elif line.find("aircraft") != -1:
-                aircraft_name = line.split("aircraft")[1]
-                temp_aircraft = aircraft(aircraft_name)
-                temp.aircraft.append(temp_aircraft)
-        if temp.name != "temp":
-            locations.append(temp)
-        '''
-                
-        return self.locations,self.flights
                                
-x = parser()
-loc,flt = x.load("scenarios/test.scn")
-for item in loc:
-    print(item)
+x = parser("scenarios/test.scn")
+
                 
                 
                     
